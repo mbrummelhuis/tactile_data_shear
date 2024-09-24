502444621b5358e4f85b8b4a48c4b6fe53b9d9a4
Martijn Brummelhuis <mbrummelhuis@gmail.com>
2024-09-24 10:54:37 +0100

    Removed relative pathing in favour of absolute pathing

--- tactile_data_shear/tactile_servo_control.py
+++ tactile_data_shear/tactile_servo_control.py
@@ -2,6 +2,6 @@ import os
 
 BASE_PATH = os.path.dirname(__file__)
 
-BASE_DATA_PATH = os.path.join(BASE_PATH, '..', 'data')
-BASE_MODEL_PATH = os.path.join(BASE_PATH, '..', 'models')
-BASE_RUNS_PATH = os.path.join(BASE_PATH, '..', 'runs')
+BASE_DATA_PATH = os.path.join(os.path.dirname(BASE_PATH), 'data')
+BASE_MODEL_PATH = os.path.join(os.path.dirname(BASE_PATH), 'models')
+BASE_RUNS_PATH = os.path.join(os.path.dirname(BASE_PATH), 'runs')
